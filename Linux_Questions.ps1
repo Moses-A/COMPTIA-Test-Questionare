b33ba3fca3ffdd8727f1937e2f22230f72ddc954
Moses Arocha <carocha24@gmail.com>
2017-02-25 23:29:52 -0600

    Incomplete

--- Linux_Questions.ps1
+++ Linux_Questions.ps1
@@ -1,1 +1,26 @@
 #Simple Multiple Choice Questionare   
+# Written by Moses Arocha
+
+$Entry_message = " Welcome To The Linux Questionare"
+$answer = " Would You Like To Go Over Notes? "
+
+$yes = New-Object System.Management.Automation.Host.ChoiceDescription "&Yes","Preparing Notes..."
+$no = New-Object System.Management.Automation.Host.ChoiceDescription "&No", " Preparing Exam Questions"
+
+$options = [System.Management.Automation.Host.ChoiceDescription[]] ($yes, $no)
+
+$result = $host.ui.PromptForChoice($Entry_message, $answer, $options, 0)
+
+switch ($result)
+{
+    0 {
+        $notes = Get-Content "C:\Users\localhost\Documents\Linux+\Help Facts.txt" | ForEach-Object {Get-WmiObject -computername $_ win32_bios}
+
+    }
+
+    1{
+        
+        
+
+    }
+}
